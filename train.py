59ac285b4a0d9552e178b8dd9b2cf361fd803241
Owen Van Esbroeck <mofvanes@gmail.com>
2026-02-05 15:55:19 -0500

    added replay

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ Continual Pretraining Pipeline
 """
 
 import os
+import random
 import json
 import torch
 import argparse
@@ -221,21 +222,22 @@ class DataProcessor:
 
 class ChatProcessor:
     """Data processor for instruction tuning. Input is chat template"""
-    def __init__(self, tokenizer, config: DataConfig):
+    def __init__(self, tokenizer, config: DataConfig, replay=False):
         self.tokenizer = tokenizer
         if self.tokenizer.chat_template is None:
             logger.info("No default chat template. Setting one")
             self.tokenizer.chat_template = "{{- bos_token }}\n{%- if custom_tools is defined %}\n    {%- set tools = custom_tools %}\n{%- endif %}\n{%- if not tools_in_user_message is defined %}\n    {%- set tools_in_user_message = true %}\n{%- endif %}\n{%- if not date_string is defined %}\n    {%- set date_string = \"26 Jul 2024\" %}\n{%- endif %}\n{%- if not tools is defined %}\n    {%- set tools = none %}\n{%- endif %}\n\n{#- This block extracts the system message, so we can slot it into the right place. #}\n{%- if messages[0]['role'] == 'system' %}\n    {%- set system_message = messages[0]['content']|trim %}\n    {%- set messages = messages[1:] %}\n{%- else %}\n    {%- set system_message = \"\" %}\n{%- endif %}\n\n{#- System message + builtin tools #}\n{{- \"<|start_header_id|>system<|end_header_id|>\\n\\n\" }}\n{%- if builtin_tools is defined or tools is not none %}\n    {{- \"Environment: ipython\\n\" }}\n{%- endif %}\n{%- if builtin_tools is defined %}\n    {{- \"Tools: \" + builtin_tools | reject('equalto', 'code_interpreter') | join(\", \") + \"\\n\\n\"}}\n{%- endif %}\n{{- \"Cutting Knowledge Date: December 2023\\n\" }}\n{{- \"Today Date: \" + date_string + \"\\n\\n\" }}\n{%- if tools is not none and not tools_in_user_message %}\n    {{- \"You have access to the following functions. To call a function, please respond with JSON for a function call.\" }}\n    {{- 'Respond in the format {\"name\": function name, \"parameters\": dictionary of argument name and its value}.' }}\n    {{- \"Do not use variables.\\n\\n\" }}\n    {%- for t in tools %}\n        {{- t | tojson(indent=4) }}\n        {{- \"\\n\\n\" }}\n    {%- endfor %}\n{%- endif %}\n{{- system_message }}\n{{- \"<|eot_id|>\" }}\n\n{#- Custom tools are passed in a user message with some extra guidance #}\n{%- if tools_in_user_message and not tools is none %}\n    {#- Extract the first user message so we can plug it in here #}\n    {%- if messages | length != 0 %}\n        {%- set first_user_message = messages[0]['content']|trim %}\n        {%- set messages = messages[1:] %}\n    {%- else %}\n        {{- raise_exception(\"Cannot put tools in the first user message when there's no first user message!\") }}\n{%- endif %}\n    {{- '<|start_header_id|>user<|end_header_id|>\\n\\n' -}}\n    {{- \"Given the following functions, please respond with a JSON for a function call \" }}\n    {{- \"with its proper arguments that best answers the given prompt.\\n\\n\" }}\n    {{- 'Respond in the format {\"name\": function name, \"parameters\": dictionary of argument name and its value}.' }}\n    {{- \"Do not use variables.\\n\\n\" }}\n    {%- for t in tools %}\n        {{- t | tojson(indent=4) }}\n        {{- \"\\n\\n\" }}\n    {%- endfor %}\n    {{- first_user_message + \"<|eot_id|>\"}}\n{%- endif %}\n\n{%- for message in messages %}\n    {%- if not (message.role == 'ipython' or message.role == 'tool' or 'tool_calls' in message) %}\n        {{- '<|start_header_id|>' + message['role'] + '<|end_header_id|>\\n\\n'+ message['content'] | trim + '<|eot_id|>' }}\n    {%- elif 'tool_calls' in message %}\n        {%- if not message.tool_calls|length == 1 %}\n            {{- raise_exception(\"This model only supports single tool-calls at once!\") }}\n        {%- endif %}\n        {%- set tool_call = message.tool_calls[0].function %}\n        {%- if builtin_tools is defined and tool_call.name in builtin_tools %}\n            {{- '<|start_header_id|>assistant<|end_header_id|>\\n\\n' -}}\n            {{- \"<|python_tag|>\" + tool_call.name + \".call(\" }}\n            {%- for arg_name, arg_val in tool_call.arguments | items %}\n                {{- arg_name + '=\"' + arg_val + '\"' }}\n                {%- if not loop.last %}\n                    {{- \", \" }}\n                {%- endif %}\n                {%- endfor %}\n            {{- \")\" }}\n        {%- else  %}\n            {{- '<|start_header_id|>assistant<|end_header_id|>\\n\\n' -}}\n            {{- '{\"name\": \"' + tool_call.name + '\", ' }}\n            {{- '\"parameters\": ' }}\n            {{- tool_call.arguments | tojson }}\n            {{- \"}\" }}\n        {%- endif %}\n        {%- if builtin_tools is defined %}\n            {#- This means we're in ipython mode #}\n            {{- \"<|eom_id|>\" }}\n        {%- else %}\n            {{- \"<|eot_id|>\" }}\n        {%- endif %}\n    {%- elif message.role == \"tool\" or message.role == \"ipython\" %}\n        {{- \"<|start_header_id|>ipython<|end_header_id|>\\n\\n\" }}\n        {%- if message.content is mapping or message.content is iterable %}\n            {{- message.content | tojson }}\n        {%- else %}\n            {{- message.content }}\n        {%- endif %}\n        {{- \"<|eot_id|>\" }}\n    {%- endif %}\n{%- endfor %}\n{%- if add_generation_prompt %}\n    {{- '<|start_header_id|>assistant<|end_header_id|>\\n\\n' }}\n{%- endif %}\n"
         self.config = config
 
-    def load_text_data(self, file_path: str) -> List[str]:
+    def load_text_data(self, file_path: str, replay_path=None, replay_percentage=0.01) -> List[str]:
         """Load and lightly clean text data"""
-        logger.info(f"Loading data from {file_path}")
+        logger.info(f"Loading instruction examples from {file_path}")
         
         with open(file_path, 'r', encoding='utf-8') as f:
             lines = f.readlines()
         
         texts = []
+        replay_texts = []
         for line in lines:
             if not line:
                 continue
@@ -244,6 +246,32 @@ class ChatProcessor:
             texts.append(chat_input)
         
         logger.info(f"Loaded {len(lines)} instruction examples")
+        
+        if replay_path is not None:
+            logger.info(f"Loading replay data from {replay_path}")
+        
+            with open(file_path, 'r', encoding='utf-8') as f:
+                replay_lines = f.readlines()
+
+            for line in replay_lines:
+                line = line.strip()
+            
+                if not line:
+                    continue
+                line = line.replace('\ufeff', '')  # Remove BOM
+                line = ' '.join(line.split())      # Normalize whitespace
+                line = unicodedata.normalize("NFC", line)
+
+                if len(line) >= 10:
+                    replay_texts.append(line)
+            
+            logger.info(f"Loaded {len(replay_texts)} texts")
+            replay_texts = replay_texts[:len(len(replay_texts)*replay_percentage)]
+            logger.info(f"Using {len(replay_texts)}")
+
+            logger.info(f"Merging with instructions")
+            texts = random.shuffle([*texts, *replay_texts])
+
         return texts
     
     def group_texts(self, examples):
